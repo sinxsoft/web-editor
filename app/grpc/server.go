61d326dfdb63dffaf90f5be7735ce1cf2944f4fe
henrik <xiaxilin@xgqq.com>
2018-01-18 14:29:21 +0800

    增加测试grpc

---
+++ app/grpc/server.go
@@ -0,0 +1,34 @@
+package grpc
+
+// server.go
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/sinxsoft/web-editor/app/grpc"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	//pb "helloworld/helloworld"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct{}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatal("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	s.Serve(lis)
+}
